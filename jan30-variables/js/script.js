77bf28bf31a0d547db2a961f5e2767926a6b8ad1
Gus <geekwise.gustavo.jimenez@gmail.com>
2017-01-31 15:36:15 -0800

    portfolio is index

---
+++ jan30-variables/js/script.js
@@ -0,0 +1,19 @@
+//alert('this is an alert');
+var myNumberVar = 9; //number=numbers
+var myStringVar = 'Kevin Henderson';//string=words
+var myBooleanVar = true;//boolean=true/false
+var myFloatVar = 5.2;//float=decimal
+var myArrayVar = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+//array=words
+var myObjectVar = {
+  make: 'Saturn',
+  model: 'Sc1',
+  color: 'silver',
+  year: 2001 };
+//object=stuff
+console.log(myNumberVar);
+console.log(myStringVar);
+console.log(myBooleanVar);
+console.log(myFloatVar);
+console.log(myArrayVar);
+console.log(myObjectVar.model);
