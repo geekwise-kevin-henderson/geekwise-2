da7648c2f7c6eb22cff5da155999dfcad6ef4eed
Gus <geekwise.gustavo.jimenez@gmail.com>
2017-02-01 15:52:35 -0800

    js-math

---
+++ feb01-js-math/js/script.js
@@ -0,0 +1,29 @@
+/* object
+array
+string
+number
+float
+boolean */
+var object = {type1:'a', type2:'b'};
+var array = ['car', 'truck', 'van'];
+var string = "car";
+var number = 1;
+var float = 1.2;
+var boolean = (7 > 3);
+var x = 1;
+var y = 2;
+var z = x + y;
+var z2 = x - y;
+var z3 = x * y;
+var z4 = x / y;
+
+console.log(object);
+console.log(array);
+console.log(string);
+console.log(number);
+console.log(float);
+console.log(boolean);
+console.log(z);
+console.log(z2);
+console.log(z3);
+console.log(z4);
