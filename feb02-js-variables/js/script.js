1314492f45b80d2f8740b0ce49e63858e7eb180e
Gus <geekwise.gustavo.jimenez@gmail.com>
2017-02-03 15:48:21 -0800

    feb03-js-strings

---
+++ feb02-js-variables/js/script.js
@@ -0,0 +1,16 @@
+var myNumberObject = 3;
+
+var myNewNumberObject = myNumberObject.toString();
+
+// console.log(myNumberObject);
+// console.log(myNewNumberObject);
+var myStringObject = "This is a string object";
+var myOtherStringObject = "This is also a string object";
+
+console.log(myStringObject, myOtherStringObject);
+
+var a = 5;
+var b = 10;
+var c = a + b;
+
+console.log(c);
---
+++ feb03-js-strings/js/script.js
@@ -0,0 +1,9 @@
+var firstName = "Kevin";
+var secondName = "James";
+var thirdName = "Henderson";
+var birthday = "December 29, 1998";
+var futureAge = 2020 - 1998;
+
+var answer = "My name is " + firstName + " " + secondName + " " + thirdName + ".  I was born on " + birthday + " and in 2020, I will be " + futureAge + ".";
+
+console.log(answer);
