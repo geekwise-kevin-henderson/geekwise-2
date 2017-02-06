f038f5fbac0ea006dfb741162140168344e58b95
Gus <geekwise.gustavo.jimenez@gmail.com>
2017-02-06 15:50:02 -0800

    feb06

---
+++ feb06-js-madlibs/js/script.js
@@ -0,0 +1,7 @@
+prompt()
+
+
+
+var finished = "To " + verb1 ", or not to " + verb1 + ", --that is the question:--Whether `tis more " + adjective1 + " in the mind to " + verb2 + " The " + noun-plural1 + " and " + weapon-plural1 + " of " + adjective2 + " fortune. Or to take " + noun1 + " against a sea of " + food-plural1 + " ,And by opposing end them?--To die,--to " + verb2 + " ,--No more; and by a " + verb2 + " to say we end The heartache, and the thousand " + adjective3 + " shocks That flesh is heir to,--`tis a " + noun2 + " Devoutly to be wish`d. To die,--to " +  verb3 + " ;--To " + verb3 + " ! perchance to dream:--ay, there`s the " + noun3 + "; For in that " + verb3 + " of death what dreams may come, When we have " + verb-ending-in-ed1 + " off this mortal " + noun4 + ", Must give us pause: there`s the respect. That makes calamity of so " + adjective4 + " life... ";
+
+alert(finished)
---
+++ feb06-js-numbers/js/script.js
@@ -0,0 +1,13 @@
+// var userLang = confirm('Do you speak it?')
+// console.log(userLang);
+// var ravioli = prompt('What is your favorite flavor of ravioli?');
+// console.log(ravioli);
+
+// var firstName = prompt('What is your first name?');
+// var hair = prompt('What color is your hair?');
+// var pizza = prompt('What is your favorite kind of pizza?')
+//
+// console.log(firstName + " has " + hair + " hair. And likes " + pizza + " pizza");
+
+var age = prompt("AGE");
+console.log(parseInt(age))
