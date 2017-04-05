1648c9c91dba3bbdb2cb099a2c734d6ff81d9bfc
Gus <geekwise.gustavo.jimenez@gmail.com>
2017-04-04 21:11:40 -0700

    4-4-17

---
+++ mar31-js-themoviedb/js/script.js
@@ -0,0 +1,77 @@
+(function(){
+  getReq('env.json', setVars);
+}());
+
+function getReq(url, cb){
+  var req = new XMLHttpRequest();
+  req.open('GET', url);
+  req.onload = function(){
+    if(req.readyState === 4 && req.status === 200){
+      cb(JSON.parse(req.responseText));
+    }else{
+      console.log('error', req.statusText);
+    }
+  }
+  req.send(null);
+}
+
+var myVars;
+
+function setVars(data){
+  myVars = envVars();
+  console.log(myVars.get_url());
+  myVars.set_api_key(data.api_key);
+  myVars.set_read_access(data.read_access);
+  console.log(myVars.get_api_key());
+  console.log(myVars.get_read_access());
+  getMovies();
+}
+
+function getMovies(){
+  var url = myVars.get_url() + "3/search/movie?api_key=" + myVars.get_api_key() + '&language=en-US&page=1&query=resident';
+  getReq(url, parseMovies);
+}
+
+function parseMovies(movies){
+  var main = document.querySelector('main');
+  movies.results.forEach(function(e,i){
+  var  section = document.createElement('section'),
+    poster_path = document.createElement('img'),
+    title = document.createElement('h1'),
+    tagline = document.createElement('h3'),
+    overview = document.createElement('p'),
+    homepage = document.createElement('a');
+
+    poster_path.src = 'http://image.tmdb.org/t/p/w500' + m.poster_path;
+    title.textContent = e.title;
+    tagline.textContent = e.tagline;
+    overview.textContent = e.overview;
+    homepage.href = e.homepage;
+    homepage.textContent = "<<learn more>>";
+
+    section.append( poster_path, title, tagline, overview, homepage);
+    main.append(section);
+  });
+
+}
+
+function envVars(){
+  var url = 'https://api.themoviedb.org/', api_key, read_access;
+  return {
+    set_api_key: function(data){
+      api_key = data;
+    },
+    set_read_access: function(data){
+      read_access = data;
+    },
+    get_api_key: function(){
+      return api_key;
+    },
+    get_read_access: function(){
+      return read_access;
+    },
+    get_url: function(){
+      return url;
+    }
+  };
+}
